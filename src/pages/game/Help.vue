c0c66510fd591208b0adb1f33e815835a64b1f86
Jonas L <joaohccl@hotmail.com>
2020-07-28 14:28:27 +0900

    fixing bug with delay changes in css

---
+++ src/pages/game/Help.vue
@@ -0,0 +1,43 @@
+<template>
+    <f7-popup id="help-page" ref="helpPopUp">
+        <div v-if="!hasVerified">
+            <h3>
+                Do you really want to select {{helpType}} ?
+            </h3>
+            <f7-button @click="hasVerified = true">
+                Yes
+            </f7-button>
+            <f7-button @click="retBack()">
+                No
+            </f7-button>
+        </div>
+        <div v-else>
+            <v
+        </div>
+    </f7-popup>
+</template>
+
+<script>
+    export default {
+        name: "Help",
+
+        props: ['helpType', 'answers'],
+
+        data() {
+            return {
+                hasVerified: false
+            }
+        },
+
+        methods: {
+            retBack() {
+                // this.$f7router.back()
+                this.$refs.helpPopUp.close();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
