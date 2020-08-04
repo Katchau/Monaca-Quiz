c1b1b9586192675ed58cd95050eb79b7c0d5b6a2
João <joaohccl@hotmail.com>
2020-08-04 17:31:08 +0900

    quite possibly the worst fix in history

--- src/pages/GameManager.vue
+++ src/pages/GameManager.vue
@@ -1,23 +1,66 @@
 <template>
     <f7-page>
-        <div id="game">
+        <div v-if="gameState === 'create'" id="game-create">
+            <f7-list form>
+                <!--  this could have smart select if we want more than 1 option               -->
+                <f7-list-input
+                    label="Select Genre"
+                    name="genre"
+                    placeholder=""
+                    type="select"
+                    default-value="any"
+                >
+                    <option value="any">Any Kind</option>
+                    <option v-for="(category, i) in categories" :key="i" :value="category.val">
+                        {{category.text}}
+                    </option>
+                </f7-list-input>
+
+                <f7-list-input
+                        label="Select Difficulty"
+                        name="difficulty"
+                        placeholder="Please choose"
+                        type="select"
+                        default-value="easy"
+                >
+                    <option value="easy">Easy</option>
+                    <option value="normal">Normal</option>
+                    <option value="hard">Hard</option>
+                </f7-list-input>
+
+                <f7-list-button @click="startGame()" title="Start Game" />
+            </f7-list>
+        </div>
+        <div v-else id="game">
+            ter aqui uma bolinha a girar
         </div>
     </f7-page>
 </template>
 
 <script>
     import MoneyTree from "@/pages/game/MoneyTree";
+    import Question from "@/pages/game/Question";
+    import End from "@/pages/game/End";
 
     export default {
         name: "GameManager",
 
         components: {
            MoneyTree,
+            Question,
+            End
         },
 
         data() {
             return {
-                gameState: 'next', //check playing loose win next stop
+                difficulty: 0, //0 - easy, 1 - normal, 2 - hard
+                categories: [
+                    {
+                        val: 'music',
+                        text: 'Music'
+                    }
+                ],
+                gameState: 'create', //create check play loose win next stop
                 currentLevelState: 1,
                 //this is just an example, the goal is to push as you go along
                 questions: [{
@@ -30,10 +73,6 @@
                     ]
                 }]
             }
-        },
-
-        beforeMount () {
-            this.transitionState()
         },
 
         methods: {
@@ -62,11 +101,17 @@
                 }
             },
 
+            startGame(){
+                // TODO check difficulty and that stuff
+                this.transitionState()
+            },
+
             transitionAsyncTimer () {
                 let self = this;
                 let timeOutFunc = function () {
-                    // self.$f7.views.main.router.back();
-                    self.transitionState();
+                    self.$f7router.back();
+                    setTimeout(()=> {self.transitionState()},2000);
+                    //self.transitionState();
                 };
 
                 setTimeout(timeOutFunc, 2000)
@@ -75,7 +120,7 @@
             transitionState() {
                 console.log(`current state: ${this.gameState}`)
                 if (this.gameState === 'check') {
-                    this.gameState = 'playing';
+                    this.gameState = 'play';
                     let question = this.getQuestion();
                     this.$f7.views.main.router.navigate('/game/question', {
                         props: {
@@ -83,8 +128,7 @@
                             answers: question.answers
                         }
                     });
-
-                } else if (this.gameState === 'playing') {
+                } else if (this.gameState === 'play') {
                     //TODO check on how to get if the answer was correct or not
                 } else {
                     //TODO por enquanto esta so o next
